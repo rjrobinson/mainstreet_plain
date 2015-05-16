60913fe6c977d4faa00cb441d67a111725ce3520
RJ Robinson <robertj.robinson@gmail.com>
2015-05-16 10:28:38 -0400

    fixed owl slider

--- js/script.js
+++ js/script.js
@@ -1,26 +1,26 @@
-$(window).load(function() {
+// $(window).load(function() {
 
-    var theWindow = $(window),
-        $bg = $("#bg"),
-        aspectRatio = $bg.width() / $bg.height();
+//     var theWindow = $(window),
+//         $bg = $("#bg"),
+//         aspectRatio = $bg.width() / $bg.height();
 
-    function resizeBg() {
+//     function resizeBg() {
 
-        if ((theWindow.width() / theWindow.height()) < aspectRatio) {
-            $bg
-                .removeClass()
-                .addClass('bgheight');
-        } else {
-            $bg
-                .removeClass()
-                .addClass('bgwidth');
-        }
+//         if ((theWindow.width() / theWindow.height()) < aspectRatio) {
+//             $bg
+//                 .removeClass()
+//                 .addClass('bgheight');
+//         } else {
+//             $bg
+//                 .removeClass()
+//                 .addClass('bgwidth');
+//         }
 
-    }
+//     }
 
-    theWindow.resize(resizeBg).trigger("resize");
+//     theWindow.resize(resizeBg).trigger("resize");
 
-});
+// });
 
 
 $(document).ready(function() {
@@ -40,5 +40,13 @@ $(document).ready(function() {
         // itemsMobile : false
 
     });
+
+    $("h2")
+        .wrapInner("<span>")
+
+    $("h2 br")
+        .before("<span class='spacer'>")
+        .after("<span class='spacer'>");
+
 
 });
