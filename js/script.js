434537652752d4b321f493186e3754c79494362f
RJ Robinson <robertj.robinson@gmail.com>
2015-05-18 10:26:39 -0400

    addid it

--- js/script.js
+++ js/script.js
@@ -1,36 +1,15 @@
-// $(window).load(function() {
-
-//     var theWindow = $(window),
-//         $bg = $("#bg"),
-//         aspectRatio = $bg.width() / $bg.height();
-
-//     function resizeBg() {
-
-//         if ((theWindow.width() / theWindow.height()) < aspectRatio) {
-//             $bg
-//                 .removeClass()
-//                 .addClass('bgheight');
-//         } else {
-//             $bg
-//                 .removeClass()
-//                 .addClass('bgwidth');
-//         }
-
-//     }
-
-//     theWindow.resize(resizeBg).trigger("resize");
-
-// });
-
-
 $(document).ready(function() {
 
     $("#owl-demo").owlCarousel({
 
         navigation: true, // Show next and prev buttons
         slideSpeed: 300,
+        pagination: true,
         paginationSpeed: 400,
-        singleItem: true
+        singleItem: true,
+        lazyLoad: true,
+        mouseDrag: true,
+        touchDrag: true
 
         // "singleItem:true" is a shortcut for:
         // items : 1,
@@ -39,6 +18,11 @@ $(document).ready(function() {
         // itemsTablet: false,
         // itemsMobile : false
 
+    });
+
+
+    $('#myTabs').on('toggled', function(event, tab) {
+        console.log(tab);
     });
 
     $("h2")
